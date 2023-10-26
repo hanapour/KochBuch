bc79683375e98a73c5e0f9c0f437dcbd439f7120
808ket <ketelhohnkilian@gmail.com>
2023-10-26 11:06:05 +0200

    Änderung26102023

---
+++ src/main/java/com/example/teszo/MainApplication.java
@@ -0,0 +1,37 @@
+package com.example.teszo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("KochBuch App");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartView.fxml"));
+        // Create the main layout using BorderPane
+       // BorderPane mainLayout = new BorderPane();
+
+
+        // Add Logo (center)
+        // Add Favorite and Category Icons (right)
+        // You can use HBox or VBox for these sections
+
+        // Create the scene and set it in the stage
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint("Hello , Um exit drucke bitte die 'esc' taste");
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.keyCombination("esc"));
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
--- src/main/java/com/example/teszo/StartController.java
+++ src/main/java/com/example/teszo/StartController.java
@@ -3,7 +3,7 @@ package com.example.teszo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StartController {
     @FXML
     private Label welcomeText;
 
