45cd7261ebc1f3d66e8b625ab46508e4bc37a693
wfahed <wessam92fahed@gmail.com>
2023-10-29 14:46:57 +0100

    Wessam, ich habe die Schaltflächen "menuTaste" und "kategorieTaste" mit dem Controller verknüpft. Ihr könnt nun die Ansichten erstellen, und ich werde mich um die Verknüpfung der Schaltflächen mit den entsprechenden Views kümmern.

--- src/main/java/com/example/kochBuch/StartController.java
+++ src/main/java/com/example/kochBuch/StartController.java
@@ -1,14 +1,53 @@
 package com.example.kochBuch;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class StartController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class StartController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
-}
+
+
+        @FXML
+        private ImageView btnCategory;
+
+        @FXML
+        private ImageView btnMenu;
+
+        @FXML
+        private ImageView btnVaforite;
+
+
+    @FXML
+    void ONClickSingup(MouseEvent event) throws Exception{
+        Stage stage = MainApplication.mainstage;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("register.fxml"));
+        Scene scene = new Scene((Parent)fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    @FXML
+    void OnMenuClick(MouseEvent event)throws Exception {
+        Stage stage = MainApplication.mainstage;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("categoryView.fxml"));
+        Scene scene = new Scene((Parent)fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    }
