e4cc8274b6d8d1fc74f3302267ed5f48ee510285
Kilian <ketelhohnkilian@gmail.com>
2023-10-31 19:00:46 +0100

    kategorien erweitern

---
+++ src/main/java/com/example/kochBuch/KategorienController/FleischController.java
@@ -0,0 +1,54 @@
+package com.example.kochBuch;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class FleischController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    private void loadFXML(String fxmlFileName) throws Exception {
+        Stage stage = MainApplication.mainstage;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxmlFileName));
+        Parent root = (Parent) fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void vegan(MouseEvent event)  throws Exception{
+        loadFXML("VeganView.fxml");
+    }
+    @FXML void fleisch(MouseEvent event)  throws Exception{
+        loadFXML("FleischView.fxml");
+    }
+    @FXML void seafood(MouseEvent event)  throws Exception{
+        loadFXML("SeafoodView.fxml");
+    }
+    @FXML void vorspeise(MouseEvent event)  throws Exception{
+        loadFXML("VorspeiseView.fxml");
+    }
+    @FXML void nachspeise(MouseEvent event)  throws Exception{
+        loadFXML("NachspeiseView.fxml");
+    }
+    @FXML void persisch(MouseEvent event)  throws Exception{
+        loadFXML("PersischView.fxml");
+    }
+    @FXML void OnClickBack(MouseEvent event)  throws Exception{
+        loadFXML("categoryView.fxml");
+    }
+}
