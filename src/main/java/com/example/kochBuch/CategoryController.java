0736d8e6591446bac554755137c0e9e9eb7d11ef
Kilian <ketelhohnkilian@gmail.com>
2023-10-31 19:00:31 +0100

    kategorien erweitern

--- src/main/java/com/example/kochBuch/CategoryController.java
+++ src/main/java/com/example/kochBuch/CategoryController.java
@@ -51,4 +51,36 @@ public class CategoryController implements Initializable {
 
     }
 
+    private void loadFXML(String fxmlFileName) throws Exception {
+        Stage stage = MainApplication.mainstage;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxmlFileName));
+        Parent root = (Parent) fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML void vegan(MouseEvent event)  throws Exception{
+        loadFXML("VeganView.fxml");
+    }
+    @FXML void fleisch(MouseEvent event)  throws Exception{
+        loadFXML("FleischView.fxml");
+    }
+    @FXML void seafood(MouseEvent event)  throws Exception{
+        loadFXML("SeafoodView.fxml");
+    }
+    @FXML void vorspeise(MouseEvent event)  throws Exception{
+        loadFXML("VorspeiseView.fxml");
+    }
+    @FXML void nachspeise(MouseEvent event)  throws Exception{
+        loadFXML("NachspeiseView.fxml");
+    }
+    @FXML void persisch(MouseEvent event)  throws Exception{
+        loadFXML("PersischView.fxml");
+    }
+    /*@FXML void OnClickBack(MouseEvent event)  throws Exception{
+        loadFXML("StartView.fxml");
+    }*/
+
 }
