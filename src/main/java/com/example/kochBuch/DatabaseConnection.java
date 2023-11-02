5785d9370ac57eb2589f0d69f886b271738101dc
wfahed <wessam92fahed@gmail.com>
2023-11-02 10:21:33 +0100

    DatenBanK Und AnmeldenClass

---
+++ src/main/java/com/example/kochBuch/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package com.example.kochBuch;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // MySQL-Datenbankverbindungsinformationen
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/kochbuch";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "12345678";
+
+
+    // Methode zum Herstellen der Verbindung zur MySQL-Datenbank
+    public Connection connectToDatabase() {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            System.out.println("Verbindung zur MySQL-Datenbank hergestellt.");
+            return connection;
+        } catch (SQLException e) {
+            System.err.println("Fehler beim Herstellen der Verbindung zur MySQL-Datenbank: " + e.getMessage());
+            return null;
+        }
+
+        //
+    }
+}
